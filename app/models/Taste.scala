888719b4297193fdd68de096ac06fa5f11a0608c
guyuanerhi <guyuanershi@gmail.com>
2014-02-26 16:16:30 +0800

    change twe to wte

--- app/models/Taste.scala
+++ app/models/Taste.scala
@@ -3,6 +3,9 @@ package models
 /**
  * Created by guyuanershi on 2/26/14.
  */
+
+import com.novus.salat._
+
 case class Taste (
   name: String
 )
--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
-name := "twe"
+name := "wte"
 
 version := "1.0-SNAPSHOT"
 
